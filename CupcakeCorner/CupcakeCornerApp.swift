4364f9689825bf21b37a162c3a75455c4cee087e
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-29 14:01:46 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Dmitry Sharabin on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
